9da651b5ad71c7881070162c4bd7513a50fcc868
mscharlock <michelle.scharlock@gmail.com>
2018-01-15 16:12:41 -0800

    Gituations

--- app.js
+++ app.js
@@ -1,29 +1,63 @@
-var builder = require('botbuilder');
+/*-----------------------------------------------------------------------------
+A simple echo bot for the Microsoft Bot Framework.
+-----------------------------------------------------------------------------*/
 
-var connector = new builder.ConsoleConnector().listen();
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+var restify = require('restify');
+var builder = require('botbuilder');
+var botbuilder_azure = require('botbuilder-azure');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(3978, function () {
+  console.log('%s listening to %s', server.name, server.url);
 });
-//
-// var restify = require('restify');
-// var builder = require('botbuilder');
-//
-// // Setup Restify Server
-// var server = restify.createServer();
-// server.listen(process.env.port || process.env.PORT || 3978, function () {
-//    console.log('%s listening to %s', server.name, server.url);
-// });
-//
-// // Create chat connector for communicating with the Bot Framework Service
-// var connector = new builder.ChatConnector({
-//     appId: process.env.MICROSOFT_APP_ID,
-//     appPassword: process.env.MICROSOFT_APP_PASSWORD
-// });
-//
-// // Listen for messages from users
-// server.post('/api/messages', connector.listen());
-//
-// // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-// var bot = new builder.UniversalBot(connector, function (session) {
-//     session.send("You said: %s", session.message.text);
-// });
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+  appId: process.env.MicrosoftAppId,
+  appPassword: process.env.MicrosoftAppPassword,
+  openIdMetadata: process.env.BotOpenIdMetadata,
+});
+
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
+/*----------------------------------------------------------------------------------------
+* Bot Storage: This is a great spot to register the private state storage for your bot.
+* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
+* ---------------------------------------------------------------------------------------- */
+
+var tableName = 'botdata';
+var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+
+// Create your bot with a function to receive messages from the user
+var bot = new builder.UniversalBot(connector);
+bot.set('storage', tableStorage);
+
+//Sales Object for Grover Products:
+var salesData = {
+  'central': {
+    units: 100,
+    total: '$3,000',
+  },
+  'east': {
+    units: 300,
+    total: '$9,000',
+  },
+};
+
+
+
+//Bot prompts
+bot.dialog('greetings', [
+  // Step 1
+  function (session) {
+    builder.Prompts.text(session, 'Hi! What is your name?');
+  },
+  // Step 2
+  function (session, results) {
+    session.endDialog(`Hello ${results.response}!`);
+  },
+]);
